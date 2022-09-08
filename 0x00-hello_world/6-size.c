1d493b182242fe7dea4c58bca8448debc7f77bdb
tobey14 <sureboytobi@gmail.com>
2022-09-08 13:58:37 -0700

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,12 @@
 
 
 /**
+ *main - print the string in the put function
  *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 
 int main(void)
 
@@ -25,9 +19,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));				printf("Size of an int: %ld byte(s)\n", sizeof(i));				printf("Size of a long int: %ld byte(s)\n", sizeof(li));			printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-											printf("Size of a float: %ld byte(s)\n", sizeof(f));
-
-												return (0);
-
+	
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));		
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));	
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }
