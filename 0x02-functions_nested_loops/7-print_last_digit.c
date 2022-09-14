1d7d28ec020b7a0c760843de1be93abc68bceb72
tobey14 <sureboytobi@gmail.com>
2022-09-14 11:28:29 -0700

    My first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
 
