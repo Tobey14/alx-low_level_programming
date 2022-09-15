ed5663d73c56a77645639c7006dd5ed2699d48c7
tobey14 <sureboytobi@gmail.com>
2022-09-15 12:38:28 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Print a line
+ * Description: The function should print a new line
+ * @n: number of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (num_lines = 1; num_lines <= n; num_lines++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
