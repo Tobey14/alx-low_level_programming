5a8868ce00854137b12b6e57505192cb19fc3f98
Onuigbo Tobechukwu Collins <sureboytobi@gmail.com>
2022-10-07 00:25:24 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -24,10 +24,9 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	for (i = 0 ; min <= max ; i++)
+	for (i = 0 ; min <= max ; min++, i++)
 	{
-		array[i] = min + i;
- 
+		array[i] = min;
 	}
 	return (array);
 }
