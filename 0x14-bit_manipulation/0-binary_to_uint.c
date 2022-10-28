52fa853eae2a59feb686e03b8aa6280060cc3b29
Tobey14 <sureboytobi@gmail.com>
2022-10-28 16:03:37 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,32 +6,32 @@
  *
  * Return: the convertd number or 0 if b is NULL or different from 0 or 1
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-    int length, i, add;
+	unsigned int usign;
+	int length, two;
 
-	add = 0;
-
-	if (b != NULL)
-	{
-		for (length = 0 ; b[length] != '\0' ; length++)
-		{
-			if (b[length] != 48 && b[length] != 49)
-			{
-				return (0);
-			}
-		}
-
-		for (i = 0, length-- ; length >= 0 ; length--)
-		{
-			add = add + ((b[length] - '0') << i);
-			i++;
-		}
-	}
-	else
-	{
+	if (!b)
 		return (0);
-	}
-	return (add);
 
+	usign = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, two = 1; len >= 0; len--, two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			usign += two;
+		}
+	}
+
+	return (usign);
 }
