eba2d2a999c57f67c4d9646720933bc7a987016b
Tobey14 <sureboytobi@gmail.com>
2022-12-08 15:59:38 +0100

    Fixes

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,12 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node != NULL & *head != NULL)
+	if (*head != NULL)
 	{
 		new_node->next = *head;
 		new_node->prev = NULL;
 		new_node->n = n;
-
+		*head = new_node;
 		return (new_node);
 	}
 	return (NULL);
