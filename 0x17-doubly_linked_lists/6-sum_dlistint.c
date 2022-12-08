62ece6bfce7281ef909476978984e76bd07091f3
Tobey14 <sureboytobi@gmail.com>
2022-12-08 16:43:23 +0100

    Sum of n

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of all data of a list
+ * @head: head of the list
+ *
+ * Return: the sum of all data in a list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+
+	}
+	return (sum);
+}
